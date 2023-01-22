ffd6b81277b5d532c75c43e1f93fa25de9ea8b31
hyunjun06 <junkayden@gmail.com>
2023-01-22 14:02:32 +0900

    finished #3

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,251 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 80,
+          backgroundColor: const Color(0xFF181818),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Text(
+                              'Hey, Selena',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              'Welcome Back',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                              ),
+                            ),
+                          ])
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    '\$5 194 482',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 42,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      RoundButton(
+                          text: 'Transfer',
+                          color: Color(0xFFF1B33B),
+                          textColor: Colors.black),
+                      RoundButton(
+                          text: 'Request',
+                          color: Color(0xFF1F2123),
+                          textColor: Colors.white),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Wallets',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'View All',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Card(
+                    currency: 'Euro',
+                    amount: '6 428',
+                    unit: 'Euro',
+                    icon: Icons.euro_sharp,
+                    isInverted: false,
+                  ),
+                  Transform.translate(
+                    offset: const Offset(0, -40),
+                    child: const Card(
+                      currency: 'Bitcoin',
+                      amount: '9 725',
+                      unit: 'Euro',
+                      icon: Icons.currency_bitcoin_sharp,
+                      isInverted: true,
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: const Offset(0, -80),
+                    child: const Card(
+                      currency: 'Dollar',
+                      amount: '428',
+                      unit: 'Euro',
+                      icon: Icons.attach_money_sharp,
+                      isInverted: false,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          )),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final String currency;
+  final String amount;
+  final String unit;
+  final IconData icon;
+  final bool isInverted;
+
+  const Card({
+    required this.currency,
+    required this.amount,
+    required this.unit,
+    required this.icon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: isInverted ? Colors.white : const Color(0xFF1F2123),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(children: const [
-                  Text(
-                    'Hey, Selena',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                Text(
+                  currency,
+                  style: TextStyle(
+                    color: isInverted ? const Color(0xFF1F2123) : Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
                   ),
-                  Text(
-                    'Welcome Back',
-                    style: TextStyle(
-                      color: Colors.white,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color:
+                            isInverted ? const Color(0xFF1F2123) : Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                ])
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      unit,
+                      style: TextStyle(
+                        color: isInverted
+                            ? const Color(0xFF1F2123).withOpacity(0.8)
+                            : Colors.white.withOpacity(0.8),
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                )
               ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                child: Icon(
+                  icon,
+                  color: isInverted ? const Color(0xFF1F2123) : Colors.white,
+                  size: 88,
+                ),
+              ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Color textColor;
+
+  const RoundButton({
+    required this.text,
+    required this.color,
+    required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 18,
+          ),
         ),
       ),
     );
