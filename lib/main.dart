b8f2c88b78ebb36fa69028f2e5758b51ba3228c5
hyunjun06 <junkayden@gmail.com>
2023-01-23 15:00:25 +0900

    Stateful Widget Study

--- lib/main.dart
+++ lib/main.dart
@@ -1,259 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            const Text(
-                              'Hey, Selena',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w800,
-                              ),
-                            ),
-                            Text(
-                              'Welcome Back',
-                              style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
-                                fontSize: 18,
-                              ),
-                            ),
-                          ])
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 70,
-                  ),
-                  Text(
-                    'Total Balance',
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.8),
-                      fontSize: 22,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    '\$5 194 482',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 42,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      RoundButton(
-                          text: 'Transfer',
-                          color: Color(0xFFF1B33B),
-                          textColor: Colors.black),
-                      RoundButton(
-                          text: 'Request',
-                          color: Color(0xFF1F2123),
-                          textColor: Colors.white),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'Wallets',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        'View All',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Card(
-                    currency: 'Euro',
-                    amount: '6 428',
-                    unit: 'Euro',
-                    icon: Icons.euro_sharp,
-                    isInverted: false,
-                  ),
-                  Transform.translate(
-                    offset: const Offset(0, -40),
-                    child: const Card(
-                      currency: 'Bitcoin',
-                      amount: '9 725',
-                      unit: 'Euro',
-                      icon: Icons.currency_bitcoin_sharp,
-                      isInverted: true,
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: const Offset(0, -80),
-                    child: const Card(
-                      currency: 'Dollar',
-                      amount: '428',
-                      unit: 'Euro',
-                      icon: Icons.attach_money_sharp,
-                      isInverted: false,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )),
+    return const MaterialApp(
+      home: Counter(),
     );
   }
 }
 
-class Card extends StatelessWidget {
-  final String currency;
-  final String amount;
-  final String unit;
-  final IconData icon;
-  final bool isInverted;
+class Counter extends StatefulWidget {
+  const Counter({
+    Key? key,
+  }) : super(key: key);
 
-  const Card({
-    required this.currency,
-    required this.amount,
-    required this.unit,
-    required this.icon,
-    required this.isInverted,
-  });
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  void onClick() {
+    setState(() {
+      count++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : const Color(0xFF1F2123),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  currency,
-                  style: TextStyle(
-                    color: isInverted ? const Color(0xFF1F2123) : Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color:
-                            isInverted ? const Color(0xFF1F2123) : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      unit,
-                      style: TextStyle(
-                        color: isInverted
-                            ? const Color(0xFF1F2123).withOpacity(0.8)
-                            : Colors.white.withOpacity(0.8),
-                        fontSize: 20,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                offset: const Offset(-5, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted ? const Color(0xFF1F2123) : Colors.white,
-                  size: 88,
-                ),
+            Text(
+              'Click Count',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
               ),
-            )
+            ),
+            Text('$count',
+                style:
+                    const TextStyle(fontSize: 50, fontWeight: FontWeight.w200)),
+            IconButton(
+              onPressed: onClick,
+              icon: const Icon(Icons.add_box_sharp),
+              iconSize: 50,
+              color: Colors.black,
+            ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundButton extends StatelessWidget {
-  final String text;
-  final Color color;
-  final Color textColor;
-
-  const RoundButton({
-    required this.text,
-    required this.color,
-    required this.textColor,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: textColor,
-            fontSize: 18,
-          ),
         ),
       ),
     );
